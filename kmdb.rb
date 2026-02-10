1542ded92f6ba6e254304e357ac78f6dde2c45ae
sfengxjk <sam.feng@kellogg.northwestern.edu>
2026-02-10 04:12:53 +0000

    temp save

--- kmdb.rb
+++ kmdb.rb
@@ -78,13 +78,153 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
+Agent.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
+# rails generate model Studio
+# rails generate model Movie
+# rails generate model Actor
+# rails generate model Role
+# rails generate model Agent
+# rails db:migrate
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+wb = Studio.new
+wb["name"] = "Warner Bros"
+wb.save
+
+agent_sam = Agent.new
+agent_sam["name"] = "Sam Feng"
+agent_sam.save
+
+# Inserting all movies
+batman_begins = Movie.new
+batman_begins["title"] = "Batman Begins"
+batman_begins["year_released"] = 2005
+batman_begins["rated"] = "PG-13"
+batman_begins["studio_id"] = wb.id
+batman_begins.save
+
+dark_knight_1 = Movie.new
+dark_knight_1["title"] = "The Dark Knight"
+dark_knight_1["year_released"] = 2008
+dark_knight_1["rated"] = "PG-13"
+dark_knight_1["studio_id"] = wb.id
+dark_knight_1.save
+
+dark_knight_2 = Movie.new
+dark_knight_2["title"] = "The Dark Knight Rises"
+dark_knight_2["year_released"] = 2012
+dark_knight_2["rated"] = "PG-13"
+dark_knight_2["studio_id"] = wb.id
+dark_knight_2.save
+
+# Inserting all actors/actresses
+a_cb = Actor.new
+a_cb["name"] = "Christian Bale"
+a_cb["agent_id"] = agent_sam.id
+a_cb.save
+
+a_mc = Actor.new
+a_mc["name"] = "Michael Caine"
+a_mc["agent_id"] = agent_sam.id
+a_mc.save
+
+a_ln = Actor.new
+a_ln["name"] = "Liam Neeson"
+a_ln["agent_id"] = agent_sam.id
+a_ln.save
+
+a_kh = Actor.new
+a_kh["name"] = "Katie Holmes"
+a_kh["agent_id"] = agent_sam.id
+a_kh.save
+
+a_go = Actor.new
+a_go["name"] = "Gary Oldman"
+a_go["agent_id"] = agent_sam.id
+a_go.save
+
+a_hl = Actor.new
+a_hl["name"] = "Heath Ledger"
+a_hl["agent_id"] = agent_sam.id
+a_hl.save
+
+a_ae = Actor.new
+a_ae["name"] = "Aaron Eckhart"
+a_ae["agent_id"] = agent_sam.id
+a_ae.save
+
+a_mg = Actor.new
+a_mg["name"] = "Maggie Gyllenhaal"
+a_mg["agent_id"] = agent_sam.id
+a_mg.save
+
+a_th = Actor.new
+a_th["name"] = "Tom Hardy"
+a_th["agent_id"] = agent_sam.id
+a_th.save
+
+a_jgl = Actor.new
+a_jgl["name"] = "Joseph Gordon-Levitt"
+a_jgl["agent_id"] = agent_sam.id
+a_jgl.save
+
+a_ah = Actor.new
+a_ah["name"] = "Anne Hathaway"
+a_ah["agent_id"] = agent_sam.id
+a_ah.save
+
+# Inserting roles
+r_bb_bw = Role.new
+r_bb_bw["movie_id"] = batman_begins.id
+r_bb_bw["actor_id"] = a_cb.id
+r_bb_bw["character_name"] = "Bruce Wayne"
+r_bb_bw.save
+
+r_bb_a = Role.new
+r_bb_a["movie_id"] = batman_begins.id
+r_bb_a["actor_id"] = a_mc.id
+r_bb_a["character_name"] = "Alfred"
+r_bb_a.save
+
+r_bb_rag = Role.new
+r_bb_rag["movie_id"] = batman_begins.id
+r_bb_rag["actor_id"] = a_ln.id
+r_bb_rag["character_name"] = "Ra's Al Ghul"
+r_bb_rag.save
+
+r_bb_rd = Role.new
+r_bb_rd["movie_id"] = batman_begins.id
+r_bb_rd["actor_id"] = a_kh.id
+r_bb_rd["character_name"] = "Rachel Dawes"
+r_bb_rd.save
+
+r_bb_cg = Role.new
+r_bb_cg["movie_id"] = batman_begins.id
+r_bb_cg["actor_id"] = a_go.id
+r_bb_cg["character_name"] = "Commissioner Gordon"
+r_bb_cg.save
+
+r_dk1_bw = Role.new
+r_dk1_bw["movie_id"] = dark_knight_1.id
+r_dk1_bw["actor_id"] = a_cb.id
+r_dk1_bw["character_name"] = "Bruce Wayne"
+r_dk1_bw.save
+
+r_dk1_j = Role.new
+r_dk1_j["movie_id"] = dark_knight_1.id
+r_dk1_j["actor_id"] = a_cb.id
+r_dk1_j["character_name"] = "Bruce Wayne"
+r_dk1_j.save
+
 
 # Prints a header for the movies output
 puts "Movies"
@@ -93,6 +233,15 @@ puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
+all_movies = Movie.all
+for movies in all_movies
+  title = movies["title"]
+  year_released = movies["year_released"]
+  rated = movies["rated"]
+  studio = movies["studio_id"]
+  studio_display = Studio.find_by({"id" => studio})
+  puts "#{title.ljust(25)}  #{year_released.to_s.ljust(6)}  #{rated.ljust(6)}  #{studio_display.name}"
+end
 
 # Prints a header for the cast output
 puts ""
@@ -102,6 +251,15 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
+all_cast = Role.all
+for cast in all_cast
+  movie_id = cast["movie_id"]
+  movie_name = Movie.find_by({"id" => movie_id})
+  actor_id = cast["actor_id"]
+  actor_name = Actor.find_by({"id" => actor_id})
+  character = cast["character_name"]
+  puts "#{movie_name.title.ljust(25)}   #{actor_name.name.ljust(22)}  #{character}"
+end
 
 # Prints a header for the agent's list of represented actors output
 puts ""
@@ -111,3 +269,5 @@ puts ""
 
 # Query the actor data and loop through the results to display the agent's list of represented actors output.
 # TODO!
+
+
